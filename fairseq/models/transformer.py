d4d19274c28499ddcd6983dbb2e2b457ccb03120
Alexander Lamb <lambalex@beluga1.int.ets1.calculquebec.ca>
2020-12-13 04:07:31 -0500

    Fix bug

--- fairseq/models/transformer.py
+++ fairseq/models/transformer.py
@@ -598,7 +598,7 @@ class TransformerDecoder(FairseqIncrementalDecoder):
         else:
             self.layers = nn.ModuleList([])
 
-        self.num_functions = 1
+        self.num_functions = 2
         shared_params = False
         print('sharing params across layers?', shared_params)
         print('num functions?', self.num_functions)
